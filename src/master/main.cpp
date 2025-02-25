9a756c94231ca54b94b2274462a8c38b39026396
x-vmaier <valentin16.maier@gmail.com>
2025-02-25 20:21:22 +0100

    Correct debugging location

--- src/master/main.cpp
+++ src/master/main.cpp
@@ -11,13 +11,13 @@ void loop()
 {
   unsigned int value = 1234; // e.g., 0x04 0xD2
 
+  Serial.print("Sending value: ");
+  Serial.println(value);
+
   // Broadcast message to all devices
   Wire.beginTransmission(0);
   Wire.write(highByte(value));
   Wire.write(lowByte(value));
-
-  Serial.print("Sending value: ");
-  Serial.println(value);
   uint8_t err = Wire.endTransmission();
 
   if (err != 0)
